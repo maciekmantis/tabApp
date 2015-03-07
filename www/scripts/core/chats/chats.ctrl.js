44f02719ec78789f542ac49d6d15296b0954cb99
8objects <maciej.scislowski@gmail.com>
2015-03-07 23:38:26 +0100

    Initial commit

---
+++ www/scripts/core/chats/chats.ctrl.js
@@ -0,0 +1,9 @@
+angular.module('starter')
+
+
+    .controller('ChatsCtrl', function ($scope, Chats) {
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        }
+    });
